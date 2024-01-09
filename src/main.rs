9b8aeb8e2925399d1c62a7c881f0fbee82525fbd
myFavShrimp <myfavshrimp@tuta.io>
2024-01-09 11:13:14 +0100

    Added request to agora api and response parsing

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use askama::Template;
-use axum::{response::IntoResponse, routing::get, Router};
+use axum::{http::StatusCode, response::IntoResponse, routing::get, Router};
+use serde::Deserialize;
 
 #[derive(Template)]
 #[template(path = "hello_agora.html")]
@@ -26,6 +27,56 @@ async fn main() -> eyre::Result<()> {
     Ok(())
 }
 
+#[derive(Deserialize, Debug)]
+struct AgoraApiResponseData {
+    data: Vec<(String, f64, String)>,
+    columns: Vec<String>,
+}
+
+#[derive(Deserialize, Debug)]
+struct AgoraApiResponse {
+    status: bool,
+    data: AgoraApiResponseData,
+}
+
+static AGORA_API_URL: &str = "https://api.agora-energy.org/api/raw-data";
+static AGORA_API_KEY_HEADER_NAME: &str = "api-key";
+static AGORA_API_KEY_HEADER_VALUE: &str = "agora_live_62ce76dd202927.67115829";
+
 async fn refresh_data_handler() -> impl IntoResponse {
     println!("HANNA ist doof");
+
+    let reqwest_client = reqwest::Client::new();
+
+    let agora_response = reqwest_client
+        .post(AGORA_API_URL)
+        .header(AGORA_API_KEY_HEADER_NAME, AGORA_API_KEY_HEADER_VALUE)
+        .body(
+            r#"
+            {
+            	"filters": {
+            		"from": "2014-01-01",
+            		"to": "2015-01-01",
+            		"generation": [
+            			"Wind offshore",
+            			"Wind onshore"
+            		]
+            	},
+            	"x_coordinate": "date_id",
+            	"y_coordinate": "value",
+            	"view_name": "live_gen_plus_emi_de_hourly",
+            	"kpi_name": "conventional_power_generation",
+            	"z_coordinate": "generation"
+            }
+        "#,
+        )
+        .send()
+        .await;
+
+    println!(
+        "{:#?}",
+        agora_response.unwrap().json::<AgoraApiResponse>().await
+    );
+
+    (StatusCode::OK, "ok")
 }
