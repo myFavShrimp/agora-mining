6b7343888ca0753cb8e02bc0c2164efac2544425
myFavBalu <skeyhl@gmail.com>
2024-01-30 09:43:15 +0100

    added stradh-button and graph rendering via plotters

--- src/main.rs
+++ src/main.rs
@@ -30,6 +30,7 @@ async fn main() -> eyre::Result<()> {
         .route("/", get(landing_page_handler))
         .route("/about", get(about_page_handler))
         .route("/hanna", get(refresh_data_handler))
+        .route("/graph-dracula", get(graph_handler))
         .with_state(state);
 
     let listener = tokio::net::TcpListener::bind(address).await?;
@@ -42,6 +43,42 @@ async fn landing_page_handler() -> impl IntoResponse {
     templates::HelloAgoraTemplate {
         text: "by Denis, Hanna & Lucas",
     }
+}
+
+async fn graph_handler() -> impl IntoResponse {
+    use plotters::prelude::*;
+    let mut svg_path = String::new();
+
+    {
+        let root = SVGBackend::with_string(&mut svg_path, (640, 480)).into_drawing_area();
+        root.fill(&WHITE).unwrap();
+        let mut chart = ChartBuilder::on(&root)
+            .caption("y=x^2", ("sans-serif", 50).into_font())
+            .margin(5)
+            .x_label_area_size(30)
+            .y_label_area_size(30)
+            .build_cartesian_2d(-1f32..1f32, -0.1f32..1f32).unwrap();
+
+        chart.configure_mesh().draw().unwrap();
+
+        chart
+            .draw_series(LineSeries::new(
+                (-50..=50).map(|x| x as f32 / 50.0).map(|x| (x, x * x)),
+                &RED,
+            )).unwrap()
+            .label("y = x^2")
+            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], &RED));
+
+        chart
+            .configure_series_labels()
+            .background_style(&WHITE.mix(0.8))
+            .border_style(&BLACK)
+            .draw().unwrap();
+
+        root.present().unwrap();
+
+    };
+    svg_path
 }
 
 async fn refresh_data_handler(State(state): State<Arc<AppState>>) -> impl IntoResponse {
