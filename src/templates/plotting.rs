077847d7a21256d6ab10a79df8f89a0d25689237
myFavShrimp <myfavshrimp@tuta.io>
2024-01-31 12:58:25 +0100

    Simplified db value to chart data set conversion

--- src/templates/plotting.rs
+++ src/templates/plotting.rs
@@ -42,252 +42,46 @@ impl GenerationKind {
     }
 }
 
+impl PowerGenerationAndConsumption {
+    fn get_by_kind(&self, kind: &GenerationKind) -> Option<f64> {
+        match kind {
+            GenerationKind::Biomass => self.biomass,
+            GenerationKind::GridEmissionFactor => self.grid_emission_factor,
+            GenerationKind::HardCoal => self.hard_coal,
+            GenerationKind::Hydro => self.hydro,
+            GenerationKind::Lignite => self.lignite,
+            GenerationKind::NaturalGas => self.natural_gas,
+            GenerationKind::Nuclear => self.nuclear,
+            GenerationKind::Other => self.other,
+            GenerationKind::PumpedStorageGeneration => self.pumped_storage_generation,
+            GenerationKind::Solar => self.solar,
+            GenerationKind::TotalConventionalPowerPlant => self.total_conventional_power_plant,
+            GenerationKind::TotalElectricityDemand => self.total_electricity_demand,
+            GenerationKind::TotalGridEmissions => self.total_grid_emissions,
+            GenerationKind::WindOffshore => self.wind_offshore,
+            GenerationKind::WindOnshore => self.wind_onshore,
+        }
+    }
+}
+
 pub fn to_data_sets(data: Vec<PowerGenerationAndConsumption>) -> Vec<PlottingTemplateDataSet> {
     let mut result_map: HashMap<GenerationKind, PlottingTemplateDataSet> = HashMap::new();
 
     for item in data {
         for kind in GenerationKind::all() {
-            match &kind {
-                GenerationKind::Biomass => {
-                    let new_data = PlottingTemplateDataSetData {
-                        x: item.date_id,
-                        y: item.biomass,
-                    };
+            let new_data = PlottingTemplateDataSetData {
+                x: item.date_id,
+                y: item.get_by_kind(&kind),
+            };
 
-                    if let Some(existing_data) = result_map.get_mut(&kind) {
-                        existing_data.data.push(new_data)
-                    } else {
-                        let new_data_set = PlottingTemplateDataSet {
-                            label: kind.chart_display_name(),
-                            data: vec![new_data],
-                        };
-                        result_map.insert(kind, new_data_set);
-                    }
-                }
-                GenerationKind::GridEmissionFactor => {
-                    let new_data = PlottingTemplateDataSetData {
-                        x: item.date_id,
-                        y: item.grid_emission_factor,
-                    };
-
-                    if let Some(existing_data) = result_map.get_mut(&kind) {
-                        existing_data.data.push(new_data)
-                    } else {
-                        let new_data_set = PlottingTemplateDataSet {
-                            label: kind.chart_display_name(),
-                            data: vec![new_data],
-                        };
-                        result_map.insert(kind, new_data_set);
-                    }
-                }
-                GenerationKind::HardCoal => {
-                    let new_data = PlottingTemplateDataSetData {
-                        x: item.date_id,
-                        y: item.hard_coal,
-                    };
-
-                    if let Some(existing_data) = result_map.get_mut(&kind) {
-                        existing_data.data.push(new_data)
-                    } else {
-                        let new_data_set = PlottingTemplateDataSet {
-                            label: kind.chart_display_name(),
-                            data: vec![new_data],
-                        };
-                        result_map.insert(kind, new_data_set);
-                    }
-                }
-                GenerationKind::Hydro => {
-                    let new_data = PlottingTemplateDataSetData {
-                        x: item.date_id,
-                        y: item.hydro,
-                    };
-
-                    if let Some(existing_data) = result_map.get_mut(&kind) {
-                        existing_data.data.push(new_data)
-                    } else {
-                        let new_data_set = PlottingTemplateDataSet {
-                            label: kind.chart_display_name(),
-                            data: vec![new_data],
-                        };
-                        result_map.insert(kind, new_data_set);
-                    }
-                }
-                GenerationKind::Lignite => {
-                    let new_data = PlottingTemplateDataSetData {
-                        x: item.date_id,
-                        y: item.lignite,
-                    };
-
-                    if let Some(existing_data) = result_map.get_mut(&kind) {
-                        existing_data.data.push(new_data)
-                    } else {
-                        let new_data_set = PlottingTemplateDataSet {
-                            label: kind.chart_display_name(),
-                            data: vec![new_data],
-                        };
-                        result_map.insert(kind, new_data_set);
-                    }
-                }
-                GenerationKind::NaturalGas => {
-                    let new_data = PlottingTemplateDataSetData {
-                        x: item.date_id,
-                        y: item.natural_gas,
-                    };
-
-                    if let Some(existing_data) = result_map.get_mut(&kind) {
-                        existing_data.data.push(new_data)
-                    } else {
-                        let new_data_set = PlottingTemplateDataSet {
-                            label: kind.chart_display_name(),
-                            data: vec![new_data],
-                        };
-                        result_map.insert(kind, new_data_set);
-                    }
-                }
-                GenerationKind::Nuclear => {
-                    let new_data = PlottingTemplateDataSetData {
-                        x: item.date_id,
-                        y: item.nuclear,
-                    };
-
-                    if let Some(existing_data) = result_map.get_mut(&kind) {
-                        existing_data.data.push(new_data)
-                    } else {
-                        let new_data_set = PlottingTemplateDataSet {
-                            label: kind.chart_display_name(),
-                            data: vec![new_data],
-                        };
-                        result_map.insert(kind, new_data_set);
-                    }
-                }
-                GenerationKind::Other => {
-                    let new_data = PlottingTemplateDataSetData {
-                        x: item.date_id,
-                        y: item.other,
-                    };
-
-                    if let Some(existing_data) = result_map.get_mut(&kind) {
-                        existing_data.data.push(new_data)
-                    } else {
-                        let new_data_set = PlottingTemplateDataSet {
-                            label: kind.chart_display_name(),
-                            data: vec![new_data],
-                        };
-                        result_map.insert(kind, new_data_set);
-                    }
-                }
-                GenerationKind::PumpedStorageGeneration => {
-                    let new_data = PlottingTemplateDataSetData {
-                        x: item.date_id,
-                        y: item.pumped_storage_generation,
-                    };
-
-                    if let Some(existing_data) = result_map.get_mut(&kind) {
-                        existing_data.data.push(new_data)
-                    } else {
-                        let new_data_set = PlottingTemplateDataSet {
-                            label: kind.chart_display_name(),
-                            data: vec![new_data],
-                        };
-                        result_map.insert(kind, new_data_set);
-                    }
-                }
-                GenerationKind::Solar => {
-                    let new_data = PlottingTemplateDataSetData {
-                        x: item.date_id,
-                        y: item.solar,
-                    };
-
-                    if let Some(existing_data) = result_map.get_mut(&kind) {
-                        existing_data.data.push(new_data)
-                    } else {
-                        let new_data_set = PlottingTemplateDataSet {
-                            label: kind.chart_display_name(),
-                            data: vec![new_data],
-                        };
-                        result_map.insert(kind, new_data_set);
-                    }
-                }
-                GenerationKind::TotalConventionalPowerPlant => {
-                    let new_data = PlottingTemplateDataSetData {
-                        x: item.date_id,
-                        y: item.total_conventional_power_plant,
-                    };
-
-                    if let Some(existing_data) = result_map.get_mut(&kind) {
-                        existing_data.data.push(new_data)
-                    } else {
-                        let new_data_set = PlottingTemplateDataSet {
-                            label: kind.chart_display_name(),
-                            data: vec![new_data],
-                        };
-                        result_map.insert(kind, new_data_set);
-                    }
-                }
-                GenerationKind::TotalElectricityDemand => {
-                    let new_data = PlottingTemplateDataSetData {
-                        x: item.date_id,
-                        y: item.total_electricity_demand,
-                    };
-
-                    if let Some(existing_data) = result_map.get_mut(&kind) {
-                        existing_data.data.push(new_data)
-                    } else {
-                        let new_data_set = PlottingTemplateDataSet {
-                            label: kind.chart_display_name(),
-                            data: vec![new_data],
-                        };
-                        result_map.insert(kind, new_data_set);
-                    }
-                }
-                GenerationKind::TotalGridEmissions => {
-                    let new_data = PlottingTemplateDataSetData {
-                        x: item.date_id,
-                        y: item.total_grid_emissions,
-                    };
-
-                    if let Some(existing_data) = result_map.get_mut(&kind) {
-                        existing_data.data.push(new_data)
-                    } else {
-                        let new_data_set = PlottingTemplateDataSet {
-                            label: kind.chart_display_name(),
-                            data: vec![new_data],
-                        };
-                        result_map.insert(kind, new_data_set);
-                    }
-                }
-                GenerationKind::WindOffshore => {
-                    let new_data = PlottingTemplateDataSetData {
-                        x: item.date_id,
-                        y: item.wind_offshore,
-                    };
-
-                    if let Some(existing_data) = result_map.get_mut(&kind) {
-                        existing_data.data.push(new_data)
-                    } else {
-                        let new_data_set = PlottingTemplateDataSet {
-                            label: kind.chart_display_name(),
-                            data: vec![new_data],
-                        };
-                        result_map.insert(kind, new_data_set);
-                    }
-                }
-                GenerationKind::WindOnshore => {
-                    let new_data = PlottingTemplateDataSetData {
-                        x: item.date_id,
-                        y: item.wind_onshore,
-                    };
-
-                    if let Some(existing_data) = result_map.get_mut(&kind) {
-                        existing_data.data.push(new_data)
-                    } else {
-                        let new_data_set = PlottingTemplateDataSet {
-                            label: kind.chart_display_name(),
-                            data: vec![new_data],
-                        };
-                        result_map.insert(kind, new_data_set);
-                    }
-                }
+            if let Some(existing_data) = result_map.get_mut(&kind) {
+                existing_data.data.push(new_data)
+            } else {
+                let new_data_set = PlottingTemplateDataSet {
+                    label: kind.chart_display_name(),
+                    data: vec![new_data],
+                };
+                result_map.insert(kind, new_data_set);
             }
         }
     }
