c1102423b375440cf3f6c1f5c481b71ab6f6f32c
Denis Wollgramm <myfavshrimp@tuta.io>
2024-03-18 12:38:37 +0100

    Added form data deserialization to graph endpoint

--- src/database/agora_entities.rs
+++ src/database/agora_entities.rs
@@ -1,6 +1,6 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, PartialEq, Eq)]
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq)]
 pub enum AgoraEntities {
     PowerGeneration,
     PowerEmission,
